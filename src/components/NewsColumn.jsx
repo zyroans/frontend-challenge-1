7c3364e513f48ffd1e454d0a75895579514220dd
Ciro Ibarra V <ciro.ibarrav@gmail.com>
2023-01-14 22:50:28 -0500

    resize of home and other components

--- src/components/NewsColumn.jsx
+++ src/components/NewsColumn.jsx
@@ -9,6 +9,16 @@ export default function New() {
         Will hydroigen-fueled cars ever catch up to EVs?
       </p>
       <hr className="newscolumn__linebreak" />
+      <h3 className="newscolumn__title">The Downsides of AI Artistry</h3>
+      <p className="newscolumn__content">
+        What arte the possible adverse effects of on-demand AI image generation?
+      </p>
+      <hr className="newscolumn__linebreak" />
+      <h3 className="newscolumn__title">Is VC Funding Drying Up?</h3>
+      <p className="newscolumn__content">
+        Private funding by VC firms is down 50% YOY. We take a look at what that
+        means
+      </p>
     </div>
   );
 }
